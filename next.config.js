9ccb2050dc643b266baf8ca164bde5033a7b9c30
Ryan <ryanleejwebdev@gmail.com>
2024-08-26 19:55:26 -0500

    Update config for static export and include out directory

--- next.config.js
+++ next.config.js
@@ -1,27 +1,10 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async headers() {
-      return [
-        {
-          source: '/:path*',
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
-            },
-          ],
-        },
-      ]
-    },
     output: 'export',
     images: {
-        unoptimized: true,
+      unoptimized: true,
     },
     basePath: '/bryancee-website',
   }
   
   module.exports = nextConfig
-  
